a883e292f627200089484f07b11af154c5a85049
Jorge Rios <jorgedrios86@gmail.com>
2024-07-20 12:30:34 -0300

    update- no sirvio mover archivos a public

--- src/Api/ApiHombres.js
+++ src/Api/ApiHombres.js
@@ -6,7 +6,7 @@ const apiHombres = {
       precio: 39.99,
       descripcion:
         "Camisa clásica de vestir blanca, ideal para ocasiones formales.",
-      imagen: "public/camisasHombres/camisablanca.png",
+      imagen: "src/assets/camisasHombres/camisablanca.png",
     },
     {
       id: 2,
@@ -14,7 +14,7 @@ const apiHombres = {
       precio: 29.99,
       descripcion:
         "Camisa casual de manga larga con patrón de cuadros, perfecta para un look relajado.",
-      imagen: "public/camisasHombres/camisarayada.png",
+      imagen: "src/assets/camisasHombres/camisarayada.png",
     },
     {
       id: 3,
@@ -22,7 +22,7 @@ const apiHombres = {
       precio: 34.99,
       descripcion:
         "Camisa ligera de lino en color azul, ideal para climas cálidos.",
-      imagen: "public/camisasHombres/camisalinoazul.png",
+      imagen: "src/assets/camisasHombres/camisalinoazul.png",
     },
     {
       id: 4,
