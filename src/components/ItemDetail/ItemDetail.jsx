f6143bf8dba1971134e4924b461f6bc83daa14e4
Jorge Rios <jorgedrios86@gmail.com>
2024-07-20 12:15:44 -0300

    update prueba vercel.json

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import IMG from "../Logo/portada.webp";
+import ApiHombres from "../../Api/ApiHombres";
 
-function ItemDetail({ productos }) {
+function ItemDetail() {
   const { id } = useParams();
+  const [productos, setProductos] = useState([]);
+  useEffect(() => {
+    ApiHombres().then((e) => setProductos(e.productosMasculinos));
+  }, []);
   const [item, setItem] = useState({});
 
   useEffect(
@@ -13,10 +19,13 @@ function ItemDetail({ productos }) {
   return (
     <>
       {item && (
-        <div key={item.id} className="divContainer w-full h-screen text-white bg-black pt-8">
+        <div
+          key={item.id}
+          className="divContainer w-full h-screen text-white bg-black pt-8"
+        >
           <div className=" flex flex-wrap">
             <div className="w-64 h-auto rounded-lg shadow-lg shadow-yellow-400 p-4 mx-auto m-4">
-              <img src={item.imagen} className="h-44 m-auto bg-black"></img>
+              <img src={IMG} className="h-44 m-auto bg-black"></img>
               <h1 className="text-yellow-400 my-2">U$D {item.precio}</h1>
               <p className="text-yellow-400 h-20 my-2">{item.descripcion}</p>
               <button className="text-yellow-400 my-2 rounded-lg shadow-lg shadow-yellow-400 p-2">
