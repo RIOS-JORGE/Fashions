4d5aa1bde8c498dec5a4f016a1cead5bafbf082f
Jorge Rios <jorgedrios86@gmail.com>
2024-08-04 13:03:41 -0300

    tomando URLs modificadas de Vercel o Git

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import IMG from "../../assets/camisasHombres/camisarayada.png";
+import IMG2 from "../../assets/camisasHombres/camisarayada.png";
+import IMG3 from "../../assets/camisasHombres/camisalinoazul.png";
+import IMG4 from "../../assets/camisasHombres/camisamezclilla.png";
+import IMG5 from "../../assets/camisasHombres/camisapolonegra.png";
+import IMG6 from "../../assets/pantalonesHombres/pantalónchinobeige.png";
+import IMG7 from "../../assets/pantalonesHombres/jeansslimfitoscuros.png";
+import IMG8 from "../../assets/pantalonesHombres/pantalóncargoverde.png";
+import IMG9 from "../../assets/pantalonesHombres/pantalóndevestirgris.png";
+import IMG10 from "../../assets/pantalonesHombres/shortsdeportivosnegros.png";
+import IMG11 from "../../assets/zapatosHombres/zapatosdevestirnegros.png";
+import IMG12 from "../../assets/zapatosHombres/zapatillasdeportivasblancas.png";
+import IMG13 from "../../assets/zapatosHombres/botasdecueromarrón.png";
+import IMG14 from "../../assets/zapatosHombres/mocasinesclásicos.png";
+import IMG15 from "../../assets/zapatosHombres/sandaliasdeportivas.png";
 import { AppContext } from "../AppContext/AppContext";
 
 const ItemDetail = () => {
@@ -12,7 +25,7 @@ const ItemDetail = () => {
   useEffect(() => {
     setItem(productos.find((e) => e.id === parseInt(id)))
   }, [id, productos]);
-  console.log(IMG)
+  console.log(IMG2, IMG3, IMG4, IMG5, IMG6, IMG7, IMG8, IMG9, IMG10, IMG11, IMG12, IMG13, IMG14, IMG15)
 
   return (
     <>
@@ -23,7 +36,7 @@ const ItemDetail = () => {
         >
           <div className=" flex flex-wrap">
             <div className="w-64 h-auto rounded-lg shadow-lg shadow-yellow-400 p-4 mx-auto m-4">
-              <img src={IMG} className="h-44 m-auto bg-black"></img>
+              <img src={item.imagen} className="h-44 m-auto bg-black"></img>
               <h1 className="text-yellow-400 my-2">U$D {item.precio}</h1>
               <p className="text-yellow-400 h-20 my-2">{item.descripcion}</p>
               <button className="text-yellow-400 my-2 rounded-lg shadow-lg shadow-yellow-400 p-2">
