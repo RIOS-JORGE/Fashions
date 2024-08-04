8c0052ba902821e11179a4d99a3c8837455cbd78
Jorge Rios <jorgedrios86@gmail.com>
2024-08-04 12:35:29 -0300

    upload

--- src/Api/ApiHombres.js
+++ src/Api/ApiHombres.js
@@ -6,7 +6,7 @@ const apiHombres = {
       precio: 39.99,
       descripcion:
         "Camisa clásica de vestir blanca, ideal para ocasiones formales.",
-      imagen: "/assets/camisasHombres/camisablanca-B-qEjAlw.png",
+      imagen: "/assets/camisablanca-B-qEjAlw.png",
     },
     {
       id: 2,
